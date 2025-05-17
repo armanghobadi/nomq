5e0c330ad55da248ba751d27e6f0f6ba690bedb5
Arman Ghobadi <132394098+armanghobadi@users.noreply.github.com>
2025-05-17 13:52:19 +0330

    upload stable version of NoMQ

--- NoMQ/nomq.py
+++ NoMQ/nomq.py
@@ -13,20 +13,8 @@ import select
 import json
 import gc
 
-# SimpleLogger implementation (if not provided)
-class SimpleLogger:
-    def __init__(self, level='INFO'):
-        self.level = level
-        self.levels = {'DEBUG': 0, 'INFO': 1, 'WARNING': 2, 'ERROR': 3}
-    
-    def _log(self, level, msg):
-        if self.levels.get(level, 0) >= self.levels.get(self.level, 0):
-            print(f"[{level}] {msg}")
-    
-    def info(self, msg): self._log('INFO', msg)
-    def warning(self, msg): self._log('WARNING', msg)
-    def error(self, msg): self._log('ERROR', msg)
-    def debug(self, msg): self._log('DEBUG', msg)
+#LOGGER
+from logger import SimpleLogger
 
 class NoMQ:
     """
